efeeb7662f1be9064ee2033a038e2386fa9c50c2
ProgramatorPL <32798285+ProgramatorPL@users.noreply.github.com>
2026-03-22 14:22:05 +0100

    Tiny fix

--- engine/config_gui.py
+++ engine/config_gui.py
@@ -8,7 +8,7 @@ class AppConfig:
     default_height: int = 1024
     default_steps: int = 25
     default_guidance: float = 5
-    default_scheduler: str = "Euler A"
+    default_scheduler: str = "Euler a"
     default_output: str = "output.jpg"
     max_image_size: tuple = (2048, 2048)
     preview_size: tuple = (400, 400)
